0b60093645a6c324fe86c74e1f6c65a88f92e654
amerdina <88755392+amerdina@users.noreply.github.com>
2021-09-25 12:47:00 -0400

    First commit from surface pc

---
+++ src/day30_reviewINheritunsWithQuency/GitHubTest.java
@@ -0,0 +1,10 @@
+package day30_reviewINheritunsWithQuency;
+
+public class GitHubTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("today we fix my GitHUB");
+	}
+
+}
